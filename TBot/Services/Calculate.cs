02b70eb37211dcc56e6ad59b0cc3abadbe4f8af9
Александр Полушкин <aa.polushkin@yandex.ru>
2023-02-05 20:40:14 +0300

    Внес комментарии для основного сервиса бота Calculate

--- TBot/Services/Calculate.cs
+++ TBot/Services/Calculate.cs
@@ -7,15 +7,24 @@ using System.Threading.Tasks;
 namespace TBot.Services
 {
     /// <summary>
-    /// 
+    /// Сервис реализующий интерфейс ICalculate
     /// </summary>
     public class Calculate : ICalculate
     {
+        /// <summary>
+        /// Вычисляем количество символов в сообщении
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <returns></returns>
         private string GetCountSymbol(string? message)
         {
             return $"Количество символов в строке: {message?.Length ?? 0}";
         }
-
+        /// <summary>
+        /// Вычисляем сумму чисел в сообщении
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <returns></returns>
         private string GetSumNumbers(string? message)
         {
             if (message == null) return $"Пустая строка";
@@ -36,6 +45,12 @@ namespace TBot.Services
             }
             return $"Сумма чисел = {amount}";
         }
+        /// <summary>
+        /// Метод для обработки входящего сообщения
+        /// </summary>
+        /// <param name="message">сообщение</param>
+        /// <param name="typeOperation">делегат принимающий способ обработки сообщения</param>
+        /// <returns></returns>
         private string TypeOperation(string message, Func<string, string> typeOperation) => typeOperation(message);
 
         public string GetCount(string message, string method)
